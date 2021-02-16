57afdee399511fbeb407e550c73342a4d4149be8
Tawnee Harris <tawneeh@icloud.com>
2021-02-16 15:41:39 -0800

    add notes on testing
    Co-authored-by: Sarah Gilbert <sarahgilbert064@gmail.com>
    Co-authored-by: Danielle Thompson <danithompson74@gmail.com>
    Co-authored-by: Jeff Chiu <jwchiu82@gmail.com>

--- src/_tests_/reducers/game-reducer.test.js
+++ src/_tests_/reducers/game-reducer.test.js
@@ -1,10 +1,14 @@
 import gameReducer from '../../reducers/game-reducer';
 
 describe('gameReducer', () => {
-
   test('Should return a default state if no action is passed into the reducer', () => {
     expect(gameReducer({}, {type: null})).toEqual({});
   });
 
-  
 });
+
+// testing state! don't forget
+// test for changes in state or reference to where state is set
+// can test for null
+// can test bool - is it true or false
+// can test for an empty array or elements in an array
